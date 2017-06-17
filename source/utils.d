2c7b656d09af5a7454281d005efa9afc38b2e0ca
Georg Gadinger <nilsding@nilsding.org>
2017-06-17 13:47:17 +0200

    Load assets relative to executable dir and not pwd

--- source/utils.d
+++ source/utils.d
@@ -1,7 +1,10 @@
+import std.file;
+import std.path;
+
 template LoadSprite(string name, string path)
 {
   const char[] LoadSprite =
-    "Surface surface_" ~ name ~ " = Surface(\"" ~ path ~ "\");" ~
+    "Surface surface_" ~ name ~ " = Surface(utils.pathFor(\"" ~ path ~ "\"));" ~
     "m_tex_" ~ name ~ " = Texture(surface_" ~ name ~ ");" ~
     "m_sprite_" ~ name ~ " = new Sprite(m_tex_" ~ name ~ ");";
 }
@@ -15,7 +18,12 @@ template SpriteMembers(string name)
 
 auto removeElement(R, N)(R haystack, N needle)
 {
-    import std.algorithm : countUntil, remove;
-    auto index = haystack.countUntil(needle);
-    return (index != -1) ? haystack.remove(index) : haystack;
+  import std.algorithm : countUntil, remove;
+  auto index = haystack.countUntil(needle);
+  return (index != -1) ? haystack.remove(index) : haystack;
+}
+
+auto pathFor(string path)
+{
+  return buildPath(thisExePath.dirName, path);
 }
